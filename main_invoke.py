a84a2da46a6e5544eddd0e19401a60569b22dcb9
Samir Nunes <samir.silva12342@gmail.com>
2025-06-10 18:36:46 -0300

    fix: fields for nfs

--- main_invoke.py
+++ main_invoke.py
@@ -1,5 +1,13 @@
+import yaml  # type: ignore
+
 from llm_pipeline_financial_documents import FinancialDocumentsPipeline
+
+document_name = "nota_fiscal_de_servico.txt"
+document_without_extension = document_name.split(".")[0]
 
 pipeline = FinancialDocumentsPipeline()
 
-pipeline.invoke("./documents/nota_fiscal_de_servico.pdf")
+result = pipeline.invoke("./documents/" + document_name)
+
+with open(f"result-{document_without_extension}.yaml", "w", encoding="utf-8") as f:
+    yaml.safe_dump(result, f, allow_unicode=True, default_flow_style=False)
--- src/llm_pipeline_financial_documents/models/nfs.py
+++ src/llm_pipeline_financial_documents/models/nfs.py
@@ -14,10 +14,6 @@ class NotaFiscalDeServico(BaseModel):
         ...,
         description="O código alfanumérico usado para verificar a autenticidade da NFS-e junto à prefeitura.",
     )
-    rps_substituido: str | None = Field(
-        None,
-        description="O número do Recibo Provisório de Serviços (RPS) que esta NFS-e está substituindo, se aplicável. Nulo se não houver RPS anterior.",
-    )
 
     # Dados do Prestador de Serviços
     razao_social_prestador_de_servicos: str = Field(
@@ -47,17 +43,7 @@ class NotaFiscalDeServico(BaseModel):
         ...,
         description="A Unidade Federativa (estado) onde o prestador de serviços está localizado.",
     )
-    cep_prestador: str = Field(
-        ...,
-        description="O Código de Endereçamento Postal (CEP) do prestador de serviços.",
-    )
-    telefone_prestador: str = Field(
-        ..., description="O telefone de contato do prestador de serviços."
-    )
-    email_prestador: str = Field(
-        ..., description="O e-mail de contato do prestador de serviços."
-    )
-
+    
     # Dados do Tomador de Serviços
     razao_social_tomador_de_servicos: str = Field(
         ...,
@@ -78,16 +64,6 @@ class NotaFiscalDeServico(BaseModel):
         ...,
         description="A Unidade Federativa (estado) onde o tomador de serviços está localizado.",
     )
-    cep_tomador: str = Field(
-        ...,
-        description="O Código de Endereçamento Postal (CEP) do tomador de serviços.",
-    )
-    telefone_tomador: str = Field(
-        ..., description="O telefone de contato do tomador de serviços."
-    )
-    email_tomador: str = Field(
-        ..., description="O e-mail de contato do tomador de serviços."
-    )
 
     # Detalhes dos Serviços
     codigo_servico: str = Field(
@@ -99,23 +75,13 @@ class NotaFiscalDeServico(BaseModel):
     )
 
     # Valores e Impostos
-    valor_total_dos_servicos: float = Field(
-        ..., description="O valor total bruto dos serviços prestados."
-    )
     valor_deducoes: float = Field(
         ...,
         description="O valor de deduções sobre a base de cálculo, se aplicável (ex: materiais, subempreitadas).",
     )
-    base_de_calculo_iss: float = Field(
-        ...,
-        description="O valor sobre o qual o Imposto Sobre Serviços (ISS) é calculado, após deduções.",
-    )
     aliquota_iss: float = Field(
         ...,
         description="A alíquota percentual do ISS aplicada sobre a base de cálculo.",
-    )
-    valor_iss: float = Field(
-        ..., description="O valor do Imposto Sobre Serviços (ISS) calculado."
     )
     valor_liquido: float = Field(
         ...,
@@ -125,10 +91,4 @@ class NotaFiscalDeServico(BaseModel):
     imposto_retido_na_fonte: bool = Field(
         ...,
         description="Indica se o Imposto Sobre Serviços (ISS) foi retido na fonte pelo tomador. `True` se o imposto foi retido, `False` caso contrário.",
-    )
-
-    # Informações Adicionais
-    observacoes: str = Field(
-        ...,
-        description="Campo para quaisquer observações adicionais relevantes para a nota fiscal, como condições de pagamento.",
     )
